6209128910c744db12549756f7ec4514d3cc06a7
Ann <example@mail.com>
2020-11-03 11:05:23 +0200

    first commit

---
+++ js/oldlesson/scriptles2.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let number = 4;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+// console.log(obj.name);
+console.log(obj ["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
---
+++ js/oldlesson/команды.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+
+// const answer = +prompt ("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[0] = prompt('Как ваше фамилия?', '');
+// answers[0] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+console.log(typeof(answers));
